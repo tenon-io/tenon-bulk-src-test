0596f2d62df88c5e4bd00f5b3e83028b5f53792a
Job van Achterberg <mail@3ode.nl>
2018-11-02 17:40:36 +0100

    Consistent async/await use, better logging

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 const tenonNode = require('tenon-node');
 const fs = require('fs');
-const sleep = require('sleep');
 
 const tenonApi = new tenonNode({
     key: 'ENTER YOUR API KEY HERE', // your API key
@@ -11,53 +10,58 @@ const tenonApi = new tenonNode({
 const projectID = 'ENTER YOUR PROJECT ID HERE'; // your projectID
 const filename = './queue.txt';
 
-fs.readFile(filename, 'utf8', function (err, data) {
+const runTenon = async data => {
+    const itemsOnQueue = (data.match(/(http.*)/gm) || []).length;
 
-    if (err) {
-        console.log('Error: ');
-        console.log(err);
+    if (!itemsOnQueue) {
+        console.info(`Zero items on queue - exiting.`);
+        process.exit(0);
     }
 
-    //
-    let arr = data.toString().split('\n');
-    i = arr.length;
+    console.info('Queue items remaining: ' + itemsOnQueue);
 
-    console.log('Queue Items Remaining: ' + i);
+    const url = data.match(/^([^\n]*)$/m)[1];
+    const rest = data.replace(url + (itemsOnQueue > 1 ? '\n' : ''), '');
 
-    let linesExceptFirst = data.split('\n').slice(1).join('\n');
+    console.info(`Testing: ${url}`);
 
-    console.log('Testing: ');
-    console.log(arr[0]);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    puppeteer.launch().then(async browser => {
-        const page = await browser.newPage();
+    const docID = url.replace(/^https?\:\/\/www\./, '').replace(/[^a-z0-9]+/gi, '_');
 
-        const url = arr[0];
-        const docID = url.replace(/^https?\:\/\/www\./, '').replace(/[^a-z0-9]+/gi, '_');
-
-        await page.goto(url, {
-            'waitUntil': 'networkidle2'
-        });
-
-        const content = await page.content();
-
-        tenonApi.checkSrc(content, {
-            'projectID': projectID,
-            'docID': docID
-        }, function (err, result) {
-            if (err) {
-                console.error('Error: ' + err);
-                console.log(result);
-            } else {
-                console.log('Status: ', result.status);
-                console.log(result);
-            }
-        });
-
-        fs.writeFile(filename, linesExceptFirst);
-
-        await browser.close();
-        sleep.sleep(1);
+    await page.goto(url, {
+        'waitUntil': 'networkidle2'
     });
 
+    const content = await page.content();
+
+    tenonApi.checkSrc(content, {
+        'projectID': projectID,
+        'docID': docID
+    }, (error, result) => {
+        if (error) {
+            console.error(`[Tenon] error: ${error}`);
+        } else {
+            console.info(`[Tenon] response status: ${result.status}`);
+        }
+
+        console.info(`[Tenon] result: ${JSON.stringify(result)}`);
+
+        fs.writeFile(filename, rest, async error => {
+            if (error) {
+                console.error(`Error saving queue file: ${error.message}`)
+            }
+            await browser.close();
+        });
+    });
+};
+
+fs.readFile(filename, 'utf8', (error, data) => {
+    if (error) {
+        console.error(`Error opening ${filename}: ${error.message}`);
+        process.exit(1);
+    }
+
+    runTenon(data);
 });
